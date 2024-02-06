83d3e761fc5d5cc5d91c5795dbc5f221c38ad278
hnest <david.no.1@msn.cn>
2024-02-06 22:31:13 +0800

    change to BaiduNews API

--- source/js/studyToday.js
+++ source/js/studyToday.js
@@ -15,14 +15,16 @@ new Vue({
       // 请求开源api,
       getWeiboList() {
         // fetch("https://api.emoao.com/api/60s?type=json", {
-        fetch("https://tenapi.cn/v2/zhihuhot", {
+        // fetch("https://tenapi.cn/v2/zhihuhot", {
+        // fetch("https://news.baidu.com/widget?id=hotnews&ajax=json", {
+        fetch("https://api.hnest.eu.org/get_api?url=https%3A%2F%2Fnews.baidu.com%2Fwidget%3Fid%3Dhotnews%26ajax%3Djson", {
           method: "GET", // *GET, POST, PUT, DELETE, etc.
         })
           .then((res) => {
             return res.json();
           })
           .then((data) => {
-            this.content = data.data;
+            this.content = data.data.hotnews.focusNews;
           })
           .catch((err) => {
             console.log("err", err);
